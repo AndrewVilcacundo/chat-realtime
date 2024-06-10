3fdbc4e441658d4ef9408d3591286d31786c16bd
Andrew Vilcacundo <andrewmateo15@hotmail.com>
2024-06-09 20:39:53 -0500

    Ionic-chat

---
+++ src/environments/environment.ts
@@ -0,0 +1,28 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAaU8P6uebtSpseWMdtKwEepkm1yyRWbJY",
+    authDomain: "ionic-examen-ccbb1.firebaseapp.com",
+    databaseURL: "https://ionic-examen-ccbb1-default-rtdb.firebaseio.com",
+    projectId: "ionic-examen-ccbb1",
+    storageBucket: "ionic-examen-ccbb1.appspot.com",
+    messagingSenderId: "828413525127",
+    appId: "1:828413525127:web:0962e63d594f45dd92cc22",
+    measurementId: "G-32HN6ZHTNL"
+  }
+  
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
